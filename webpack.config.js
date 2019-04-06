b30811321be431676d25dab7f2e2bed7ac012a36
NeO <wangronghui2012@hotmail.com>
2019-04-06 22:30:27 +0800

    project接口换成localhost了

--- webpack.config.js
+++ webpack.config.js
@@ -171,11 +171,11 @@ module.exports = {
         .concat([
             new webpack.DefinePlugin({
                 'process.env.NODE_ENV': '"' + (process.env.NODE_ENV || 'development') + '"',
-                'process.env.API_HOST': '"' + (process.env.API_HOST || 'http://localhost:3001') + '"',
+                'process.env.API_HOST': '"' + (process.env.API_HOST || 'http://localhost:3001/scratch-api') + '"',
                 'process.env.ASSET_HOST': '"' + (process.env.ASSET_HOST || 'https://assets.scratch.mit.edu') + '"',
                 'process.env.BACKPACK_HOST': '"' + (process.env.BACKPACK_HOST || 'https://backpack.scratch.mit.edu') + '"',
                 'process.env.CLOUDDATA_HOST': '"' + (process.env.CLOUDDATA_HOST || 'clouddata.scratch.mit.edu') + '"',
-                'process.env.PROJECT_HOST': '"' + (process.env.PROJECT_HOST || 'https://projects.scratch.mit.edu') + '"',
+                'process.env.PROJECT_HOST': '"' + (process.env.PROJECT_HOST || 'http://localhost:3001/scratch-projects') + '"',
                 'process.env.STATIC_HOST': '"' + (process.env.STATIC_HOST || 'https://cdn2.scratch.mit.edu') + '"',
                 'process.env.SCRATCH_ENV': '"' + (process.env.SCRATCH_ENV || 'development') + '"',
                 'process.env.SENTRY_DSN': '"' + (process.env.SENTRY_DSN || '') + '"'
