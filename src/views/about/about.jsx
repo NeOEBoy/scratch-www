c29a3b4634df09c6b1a9d9ddae805ae05db890de
NeO <wangronghui2012@hotmail.com>
2019-04-20 15:06:59 +0800

    小屏幕保持原有布局，不强制居中

--- src/views/about/about.jsx
+++ src/views/about/about.jsx
@@ -8,24 +8,17 @@ const Video = require('../../components/video/video.jsx');
 require('./about.scss');
 
 const About = () => (
-    <div className="inner about">
-        <h1><FormattedMessage id="general.aboutScratch" /></h1>
+  <div className="inner about">
+    <h1>关于积木编程</h1>
 
-        <div className="masthead">
-            <div>
-                <p><FormattedMessage id="about.introOne" /></p>
-                <p><FormattedMessage id="about.introTwo" /></p>
-                <p><FormattedMessage id="about.introThree" /></p>
-            </div>
-
-            <div className="video-container">
-                <Video
-                    className="about-scratch-video"
-                    videoId="sucupcznsp"
-                />
-            </div>
-        </div>
+    <div>
+      <div>
+        <p>使用 Scratch，你可以编写属于你的互动媒体，像是故事、游戏、动画，然后你可以将你的创意分享给全世界。</p>
+        <p>Scratch 帮助年轻人更具创造力、逻辑力、协作力。 这些都是生活在 21 世纪不可或缺的基本能力。</p>
+        <p>Scratch是MIT媒体实验室终生幼儿园小组开发的一个免费项目。</p>
+      </div>
     </div>
+  </div>
 );
 
 render(<Page><About /></Page>, document.getElementById('app'));
