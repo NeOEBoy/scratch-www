c96e7523805cf5fb3cbb30a75ea46f86d2f75638
NeO <wangronghui2012@hotmail.com>
2019-04-22 20:18:21 +0800

    关于文字

--- src/views/about/about.jsx
+++ src/views/about/about.jsx
@@ -9,13 +9,11 @@ require('./about.scss');
 
 const About = () => (
   <div className="inner about">
-    <h1>关于积木编程</h1>
+    <h1 style={{ padding: 10 }}>关于积木编程</h1>
 
     <div>
-      <div>
-        <p>使用 Scratch，你可以编写属于你的互动媒体，像是故事、游戏、动画，然后你可以将你的创意分享给全世界。</p>
-        <p>Scratch 帮助年轻人更具创造力、逻辑力、协作力。 这些都是生活在 21 世纪不可或缺的基本能力。</p>
-        <p>Scratch是MIT媒体实验室终生幼儿园小组开发的一个免费项目。</p>
+      <div style={{ padding: 10 }}>
+        <p>小窗口大世界，我们支持你通过Scratch创作工具，创建你自己的窗口，连接到广阔的世界。</p>
       </div>
     </div>
   </div>
