f806ca5eb673dd0602b2246703d05785be0768c3
NeO <wangronghui2012@hotmail.com>
2019-10-29 15:08:07 +0800

    增加是否微信内打开判断

--- src/init.js
+++ src/init.js
@@ -51,84 +51,89 @@ const api = require('../src/lib/api');
     );
     /* eslint-enable no-console */
 
+    let is_weixin = navigator.userAgent.toLowerCase().match(/MicroMessenger/i) == "micromessenger";
+    if (is_weixin) {
+      /**
+        * 微信配置
+        */
+      const params = { url: location.href.split('#')[0] };
+      api({
+        uri: '/wechat/sign',
+        params: params,
+        withCredentials: true
+      }, (err, res) => {
+        // console.log('/wechat/sign err = ' + err);
+        // console.log('/wechat/sign res = ' + JSON.stringify(res));
 
-    /**
-     * 微信配置
-     */
-    const params = { url: location.href.split('#')[0] };
-    api({
-      uri: '/wechat/sign',
-      params: params,
-      withCredentials: true
-    }, (err, res) => {
-      // console.log('/wechat/sign err = ' + err);
-      // console.log('/wechat/sign res = ' + JSON.stringify(res));
+        if (!err && res) {
+          /// 如果没有传递过来appid，则微信接口出错，前端不处理后续流程
+          if(!res.appId) return;
 
-      if (!err && res) {
-        /**
-         * 微信JSSDK注入配置信息
-         */
-        wx.config({
-          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-          appId: res.appId, // 必填，公众号的唯一标识
-          timestamp: res.timestamp, // 必填，生成签名的时间戳
-          nonceStr: res.nonceStr, // 必填，生成签名的随机串
-          signature: res.signature,// 必填，签名
-          jsApiList: [
-            'updateAppMessageShareData',
-            'updateTimelineShareData'
-          ] // 必填，需要使用的JS接口列表
-        });
-
-        wx.ready(function () {
           /**
-           * config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，
-           * config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，
-           * 则须把相关接口放在ready函数中调用来确保正确执行。
-           * 对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-           * */
-          // console.log('wx ready');
-
-          // 自定义“分享给朋友”及“分享到QQ”按钮的分享内容
-          wx.updateAppMessageShareData({
-            title: document.title, // 分享标题
-            desc: location.href, // 分享描述
-            link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: 'https://scratch.ruyue.xyz/favicon.ico', // 分享图标
-            success: function () {
-              // 设置成功
-              // console.log('wx.updateAppMessageShareData success');
-            },
-            fail: function (res) {
-              // 设置失败
-              // console.log('wx.updateAppMessageShareData fail');
-            }
-          });
-
-          // 自定义“分享到朋友圈”及“分享到QQ空间”按钮的分享内容
-          wx.updateTimelineShareData({
-            title: document.title, // 分享标题
-            link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: 'https://scratch.ruyue.xyz/favicon.ico', // 分享图标
-            success: function () {
-              // 设置成功
-              // console.log('wx.updateTimelineShareData success');
-            },
-            fail: function () {
-              // 设置失败
-              // console.log('wx.updateTimelineShareData fail');
-            }
-          });
-        });
-
-        wx.error(function (res) {
-          /**
-           * config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，
-           * 也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+           * 微信JSSDK注入配置信息
            */
-          // console.log('wx error res = ' + JSON.stringify(res));
-        });
-      }
-    });
+          wx.config({
+            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            appId: res.appId, // 必填，公众号的唯一标识
+            timestamp: res.timestamp, // 必填，生成签名的时间戳
+            nonceStr: res.nonceStr, // 必填，生成签名的随机串
+            signature: res.signature,// 必填，签名
+            jsApiList: [
+              'updateAppMessageShareData',
+              'updateTimelineShareData'
+            ] // 必填，需要使用的JS接口列表
+          });
+
+          wx.ready(function () {
+            /**
+             * config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，
+             * config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，
+             * 则须把相关接口放在ready函数中调用来确保正确执行。
+             * 对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+             * */
+            // console.log('wx ready');
+
+            // 自定义“分享给朋友”及“分享到QQ”按钮的分享内容
+            wx.updateAppMessageShareData({
+              title: document.title, // 分享标题
+              desc: location.href, // 分享描述
+              link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+              imgUrl: 'https://scratch.ruyue.xyz/favicon.ico', // 分享图标
+              success: function () {
+                // 设置成功
+                // console.log('wx.updateAppMessageShareData success');
+              },
+              fail: function (res) {
+                // 设置失败
+                // console.log('wx.updateAppMessageShareData fail');
+              }
+            });
+
+            // 自定义“分享到朋友圈”及“分享到QQ空间”按钮的分享内容
+            wx.updateTimelineShareData({
+              title: document.title, // 分享标题
+              link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+              imgUrl: 'https://scratch.ruyue.xyz/favicon.ico', // 分享图标
+              success: function () {
+                // 设置成功
+                // console.log('wx.updateTimelineShareData success');
+              },
+              fail: function () {
+                // 设置失败
+                // console.log('wx.updateTimelineShareData fail');
+              }
+            });
+          });
+
+          wx.error(function (res) {
+            /**
+             * config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，
+             * 也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+             */
+            // console.log('wx error res = ' + JSON.stringify(res));
+          });
+        }
+      });
+    }
   };
 })();
