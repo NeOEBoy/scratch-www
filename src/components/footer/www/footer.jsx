84155f6995d4ce9242ded2f22efb6c391eb519ff
NeO <wangronghui2012@hotmail.com>
2019-11-01 10:06:18 +0800

    footer增加备案号

--- src/components/footer/www/footer.jsx
+++ src/components/footer/www/footer.jsx
@@ -7,7 +7,11 @@ require('./footer.scss');
 const Footer = () => (
   <FooterBox>
     <div className="copyright">
-      <p>儒越编程 地址: 夏商·书香名苑·2栋2单元207室</p>
+      <div>儒越编程 地址: 夏商·书香名苑·2栋2单元207室</div>
+      <div>
+        <span>&copy; 2019 漳州儒越教育咨询服务有限公司 </span>
+        <a href="http://www.beian.miit.gov.cn">闽ICP备19021080号</a>
+      </div>
     </div>
   </FooterBox>
 );
